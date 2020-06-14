ccdf1586bea10c471a09a8e335728b9491c3eb16
henrey <pedro.novaes@nave.org.br>
2020-06-14 11:35:35 -0300

    alguns ajustes feitos

--- Assets/Scripts/Menu/menuResponsividade.cs
+++ Assets/Scripts/Menu/menuResponsividade.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class menuResponsividade : MonoBehaviour
 {
---
+++ Assets/Scripts/Menu/optionsResponsividade.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class optionsResponsividade : MonoBehaviour
+{
+    public int index = 2;
+    public KeyCode right,left,esc;
+
+    [Header("buttons")]
+    public Image cont, video, audio;
+
+    [Header("setores")]
+    public GameObject mainMenu;
+
+    void Start()
+    {       
+        cont.color = new Color32(255,255,255,255);
+        video.color = new Color32(255,255,255,150);
+        audio.color = new Color32(255,255,255,150);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(right)) {
+            if (index < 3)
+            {
+                index--;
+            }
+            else {
+                index -= 2;
+            }
+        }
+
+        if (Input.GetKeyDown(left))
+        {
+            if (index > -1)
+            {
+                index++;
+            }
+            else{
+                index += 2;
+            }
+        }
+      //print(index);
+        Selected();
+    }
+
+    void Selected (){
+      if(index == 2) {
+            cont.color = new Color32(255,255,225,255);
+            video.color = new Color32(255,255,225,150);
+            audio.color = new Color32(255,255,225,150);
+      }
+      else if(index==1){ 
+
+            cont.color = new Color32(255,255,225,150);
+            video.color = new Color32(255,255,225,255);
+            audio.color = new Color32(255,255,225,150);
+      }
+
+      else if(index==0){ 
+        
+            cont.color = new Color32(255,255,225,150);
+            video.color = new Color32(255,255,225,150);
+            audio.color = new Color32(255,255,225,255);
+      }
+    
+
+    }
+
+
+}
